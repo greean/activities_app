e408cba482344f3c23d0e73989165596cf39339c
Andy Green <aj.green07@outlook.com>
2021-10-18 22:18:13 +0100

    Creating and editing activities now persisting through to the API

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosResponse } from "axios";
+import { Activity } from "../models/activity";
+
+const sleep = (delay: number) => {              // create a time delay to provide loading indicators
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay)
+    })
+}
+
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+axios.interceptors.response.use(async response => {
+    try {                                       // use the time delay every time a response is requested
+        await sleep(1000);                      // return the response after 1 second (1,000 milliseconds)
+        return response;
+    } catch (error) {                           // deal with an error from requesting a response
+        console.log(error);
+        return await Promise.reject(error);
+    }
+});
+
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+const requests = {
+    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    del: <T> (url: string) => axios.delete<T>(url).then(responseBody),
+}
+
+const Activities = {
+    list: () => requests.get<Activity[]>('/activities'), 
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => requests.post<void>('/activities', activity),
+    update: (activity: Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.del<void>(`/activities/${id}`)
+}
+
+const agent = {
+    Activities
+}
+
+export default agent;
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
   
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data);
+    agent.Activities.list().then(response => {
+      let activities: Activity[] = [];                  // Create an empty array of type 'Activity array'
+      response.forEach(activity => {                    // for each activity in the response...
+        activity.date = activity.date.split('T')[0];    // split the date to remove the time 'T' element.
+        activities.push(activity);                      // Push the activity to the 'activities' array
+      })
+      setActivities(activities);
+      setLoading(false);
     })
   }, [])
   
@@ -35,16 +44,30 @@ function App() {
   }
 
   function handleCreateOrEditActivity(activity: Activity) {
-    activity.id 
-      ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
-      : setActivities([...activities, {...activity, id: uuid()}]);
-    setEditMode(false);
-    setSelectedActivity(activity);
+    setSubmitting(true);
+    if (activity.id) {
+      agent.Activities.update(activity).then(() => {
+        setActivities([...activities.filter(x => x.id !== activity.id), activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    } else {
+      activity.id = uuid();
+      agent.Activities.create(activity).then(() => {
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    }
   }
-
+  
   function handleDeleteActivity(id: string) {
     setActivities([...activities.filter(x => x.id !== id)])
   }
+
+if (loading) return <LoadingComponent content='Loading application' />
 
   return (
     <>
@@ -60,6 +83,7 @@ function App() {
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
+          submitting={submitting}
         />
       </Container>
     </>

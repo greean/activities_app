e31cd7ce8202cc223ef95e2a381bb9d54f59360e
Andy Green <aj.green07@outlook.com>
2021-10-22 22:35:16 +0100

    Deleting an activity using MobX

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -5,6 +5,7 @@ import {v4 as uuid} from 'uuid';
 
 export default class ActivityStore {
   activities: Activity[] = [];
+  // activityRegistry = new Map<string, Activity>();
   selectedActivity: Activity | undefined = undefined;
   editMode = false;
   loading = false;
@@ -77,6 +78,23 @@ export default class ActivityStore {
         this.activities = [...this.activities.filter(a => a.id !== activity.id), activity];
         this.selectedActivity = activity;
         this.editMode = false;
+        this.loading = false;
+      })
+    } catch (error) {
+      console.log(error);
+      runInAction(() => {
+        this.loading = false;
+      })
+    }
+  }
+
+  deleteActivity = async (id: string) => {
+    this.loading = true;
+    try {
+      await agent.Activities.delete(id);
+      runInAction(() => {
+        this.activities = [...this.activities.filter(a => a.id !== id)];
+        if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
         this.loading = false;
       })
     } catch (error) {
